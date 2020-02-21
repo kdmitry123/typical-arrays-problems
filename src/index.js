22002ad8c709ecba8d6cace5a23dcd4130ac7b39
Dmitry Karachun <karachun.dmitry@mail.ru>
2020-02-21 14:15:56 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum = array.reduce((acu, cur) => acu += cur, 0)
+    return sum/array.length;
 }
